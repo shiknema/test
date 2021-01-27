2bc3bfa8589f86c8fed6f040d3321c6eeffad158
Shikha Gupta <shiknema@gmail.com>
2021-01-27 01:02:59 +0100

    Updates

---
+++ precache-manifest.72c01ce29da4ae4d4dacc2704ba2c0c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a1c6b24d2ce7b2bf69f195a3080204e",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "1355b9980863384a7fd0",
+    "url": "/test/static/css/main.0f17a4bd.chunk.css"
+  },
+  {
+    "revision": "37d693c0db66880c19a2",
+    "url": "/test/static/js/2.b1a81836.chunk.js"
+  },
+  {
+    "revision": "1355b9980863384a7fd0",
+    "url": "/test/static/js/main.6670a8e9.chunk.js"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
